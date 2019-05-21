eaaa29ae629d87af64faefd856d605d83b656244
Anton Fedorishko <fedorishko@20digital.com.ua>
2019-05-21 17:01:45 +0300

    Latest events, Toppings sections

--- js/slider-config.js
+++ js/slider-config.js
@@ -6,9 +6,28 @@ jQuery(document).ready(function () {
     slidesToScroll: 1,
     dots:true,
     variableWidth: true,
-    arrows: false
+    arrows: false,
+    appendDots: jQuery('#services-slider-dots')
   });
-
+  jQuery('.latest-events-slider').slick({
+    infinite: false,
+    // slidesToShow: 1,
+    slidesToScroll: 1,
+    dots:true,
+    variableWidth: true,
+    arrows: false,
+    appendDots: jQuery('#latest-events-slider-dots')
+  });
+  jQuery('.toppings-slider').slick({
+    infinite: false,
+    // slidesToShow: 1,
+    slidesToScroll: 1,
+    dots:false,
+    variableWidth: true,
+    arrows: true,
+    appendArrows: jQuery('#toppings-slider-arrows'),
+    nextArrow: '<button class="slick-next slick-arrow next-slide-btn" aria-label="Next" type="button" aria-disabled="false" style="">Choose your favourite</button>',
+  });
 });
 
 
