16d61e4e2ca20698b4cd8aa4838c5c22b1e2a2b4
Anton Fedorishko <fedorishko@20digital.com.ua>
2019-05-21 11:20:14 +0300

    Heder, Services, Wonder Event Sections

---
+++ js/slider-config.js
@@ -0,0 +1,17 @@
+jQuery(document).ready(function () {
+
+  jQuery('.services-slider').slick({
+    infinite: false,
+    // slidesToShow: 1,
+    slidesToScroll: 1,
+    dots:true,
+    variableWidth: true,
+    arrows: false
+  });
+
+});
+
+
+
+
+
