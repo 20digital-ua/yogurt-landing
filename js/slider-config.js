f31adc7e67ac3abf435a9d245a78f3018368ba4f
Anton Fedorishko <fedorishko@20digital.com.ua>
2019-05-27 12:14:40 +0300

    Toppings reso

--- js/slider-config.js
+++ js/slider-config.js
@@ -1,4 +1,6 @@
 var $serviceSlider = jQuery('.services-slider');
+var $latestEventsSlider = jQuery('.latest-events-slider');
+var $toppingsSlider = jQuery('.toppings-slider');
 jQuery(document).ready(function () {
   $serviceSlider.slick({
     infinite: false,
@@ -16,7 +18,7 @@ jQuery(document).ready(function () {
       }
     ]
   });
-  jQuery('.latest-events-slider').slick({
+  $latestEventsSlider.slick({
     infinite: false,
     // slidesToShow: 1,
     slidesToScroll: 1,
@@ -54,7 +56,7 @@ jQuery(document).ready(function () {
       }
     ]
   });
-  jQuery('.toppings-slider').slick({
+  $toppingsSlider.slick({
     infinite: false,
     // slidesToShow: 1,
     slidesToScroll: 1,
@@ -63,10 +65,19 @@ jQuery(document).ready(function () {
     arrows: true,
     appendArrows: jQuery('#toppings-slider-arrows'),
     nextArrow: '<button class="slick-next slick-arrow next-slide-btn" aria-label="Next" type="button" aria-disabled="false" style="">Choose your favourite</button>',
+    mobileFirst:true,
+    responsive:[
+      {
+        breakpoint: 767,
+        settings: "unslick"
+      }
+    ]
   });
 });
 $(window).resize(function() {
   $serviceSlider.slick('resize');
+  $latestEventsSlider.slick('resize');
+  $toppingsSlider.slick('resize');
 });
 
 
